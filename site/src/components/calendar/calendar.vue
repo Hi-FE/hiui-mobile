8b1553c5bffd4316c9ea24b27593f01ced380126
lijiahao <lijiahao@hitour.cc>
2016-09-21 12:09:26 +0800

    add calendar demo

--- site/src/components/calendar/calendar.vue
+++ site/src/components/calendar/calendar.vue
@@ -1,39 +1,90 @@
 <template>
   <div class="calendar">
-    <input type="text" v-model="first_value">
-    <input type="text" v-model="sec_value">
-    <calendar    :daterange="true"
-                 :date-begin="[2016, 8, 15]"
-                 :date-end="[2017, 1, 11]"
-                 :first_value.sync="first_value"
-                 :sec_value.sync="sec_value"
-                 :is_show.sync="true"
-                 :is_single_month="is_single_month"
-                 :is_range="true"
-                 :json_data="json_data"
-                 :cur_year.sync="cur_year"
-                 :cur_month.sync="cur_month"></calendar>
+    <!-- demo1 -->
+    <div class="demo">
+      <h3>demo1:连续输出模式</h3>
+      <p class="item"><input type="text" v-model="first_value_demo1"><a class="button" @click="is_show_demo1=true">呼出日历</a></p>
+      <p class="item" v-if="is_range_demo1"><input type="text" v-model="sec_value_demo1"></p>
+      <p class="item">
+        当前日历模式: {{ is_range_demo1 ? '多选' : '单选' }}
+        <a class="button" @click="is_range_demo1 = !is_range_demo1">点击切换为{{ is_range_demo1 ? '单选' : '多选' }}</a>
+      </p>
+
+      <div class="box" v-if="is_show_demo1">
+        <calendar    :is_show.sync="is_show_demo1"
+                     :first_value.sync="first_value_demo1"
+                     :sec_value.sync="sec_value_demo1"
+                     :is_range="is_range_demo1"></calendar>
+        <div class="bottom-btn" @click="is_show_demo1 = false">close</div>
+      </div>
+    </div>
+
+    <!-- demo2 -->
+    <div class="demo">
+      <h3>demo2:单月输出模式</h3>
+      <p class="item"><input type="text" v-model="first_value_demo2">
+      <p class="item" v-if="is_range_demo2"><input type="text" v-model="sec_value_demo2"></p>
+      <p class="item">
+        当前日历模式: {{ is_range_demo2 ? '多选' : '单选' }}
+        <a class="button" @click="is_range_demo2 = !is_range_demo2">点击切换为{{ is_range_demo2 ? '单选' : '多选' }}</a>
+      </p>
+
+      <calendar    :is_show.sync="true"
+                   :first_value.sync="first_value_demo2"
+                   :sec_value.sync="sec_value_demo2"
+                   :is_range="is_range_demo2"
+                   :is_single_month="true"></calendar>
+    </div>
+
+    <!-- demo3 -->
+    <div class="demo">
+      <h3>demo3:限制日期输出模式</h3>
+      <p class="item"><input type="text" v-model="first_value_demo3">
+      <p class="item" v-if="is_range_demo3"><input type="text" v-model="sec_value_demo3"></p>
+      <p class="item">
+        当前日历模式: {{ is_range_demo3 ? '多选' : '单选' }}
+        <a class="button" @click="is_range_demo3 = !is_range_demo2">点击切换为{{ is_range_demo3 ? '单选' : '多选' }}</a>
+      </p>
+
+      <calendar    :is_show.sync="true"
+                   :daterange="true"
+                   :date-begin="[2016, 8, 10]"
+                   :date-end="[2017, 1, 11]"
+                   :first_value.sync="first_value_demo3"
+                   :sec_value.sync="sec_value_demo3"
+                   :is_range="is_range_demo3"
+                   :is_single_month="true"></calendar>
+    </div>
   </div>
 </template>
 
-<stype scoped>
-
-</stype>
+<style scoped>
+  .demo { margin: 10px; padding: 10px; border: 1px solid #eee; }
+  .button { display: inline-block; font-size: 12px; background-color: #f77; color: #fff; }
+  .item { margin-bottom: 5px; font-size: 14px; }
+  .box { padding-bottom: 40px; position: absolute; top: 0; left: 0; width: 100%; z-index: 999; }
+  .box .bottom-btn { position: fixed; bottom: 0; left: 0; width: 100%; height: 40px; line-height: 40px; text-align: center; background-color: #f77; color: #fff; z-index: 99; }
+</style>
 
 <script>
   module.exports = {
     data: function() {
       return {
-        first_value: '',
-        sec_value: '',
+        // demo1
+        first_value_demo1: '',
+        sec_value_demo1: '',
+        is_range_demo1: true,
+        is_show_demo1: false,
 
-        is_single_month: false, // 是否为单日期输出,默认为true
-        dateBegin: [],
-        dateEnd: [],
-        n_months: 3, // 连续输出模式下输出月份的个数
-        cur_year: 0, // 单月份输出模式下日历的当前年份、连续输出模式下日历的第一个年份
-        cur_month: 0, // 单月份输出模式下日历的当前月份、连续输出模式下日历的第一个月份
-        json_data: {}// 需要自定义禁用日期的object
+        // demo2
+        first_value_demo2: '',
+        sec_value_demo2: '',
+        is_range_demo2: true,
+
+        // demo3
+        first_value_demo3: '',
+        sec_value_demo3: '',
+        is_range_demo3: true
       }
     },
     components: {
